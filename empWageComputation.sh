3f169449ad3d419af3d18aca4a77d351d995b779
prasadv1998 <prasadan1720@gmail.com>
2021-09-06 18:32:31 +0530

    WageComputation for Employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,14 +4,18 @@ echo "Welcome to employee wage attendance system"
 
 #constant variable
 IS_PRESENT_FULL_TIME=1
+EMP_RATE_PER_HR=20
 #variable
 EmpCheck=$((RANDOM%2)) # 1,0
 
 
 if [ $EmpCheck -eq $IS_PRESENT_FULL_TIME ]
 then
-	echo employee is present 
+	echo employee is present
+		empHr=8 
 
 else
-	echo eployee is absent
+	echo employee is absent
+		empHr=0
 fi
+salary=$(( empHr * EMP_RATE_PER_HR ))
