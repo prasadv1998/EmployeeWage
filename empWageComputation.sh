4bf9234dc117c01be97cce00565c0afbb3bb3b38
prasadv1998 <prasadan1720@gmail.com>
2021-09-06 18:13:43 +0530

    added: feature EmpAttendance check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,3 +2,16 @@
 
 echo "Welcome to employee wage attendance system"
 
+#constant variable
+IS_PRESENT_FULL_TIME=1
+#variable
+EmpCheck=$((RANDOM%2)) # 1,0
+
+
+if [ $EmpCheck -eq $IS_PRESENT_FULL_TIME ]
+then
+	echo employee is present 
+
+else
+	echo eployee is absent
+fi
